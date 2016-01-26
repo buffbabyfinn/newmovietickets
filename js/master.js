75bd543abfd3649992ec7025e8d6171362847791
andrew cho and isabelle george <cho-andrew@hotmail.com>
2016-01-26 15:39:15 -0800

    add quantity and spec functionality

--- js/master.js
+++ js/master.js
@@ -1,16 +1,18 @@
-function Ticket(titlee,time,age) {
+function Ticket(titlee,time,age, quantity) {
   this.titlee = titlee;
   this.time = time;
   this.age = age;
+  this.quantity = quantity;
 }
 
 
 Ticket.prototype.price = function() {
-    var setTicketPrice = 10
+    var setTicketPrice = 10;
+    debugger;
     if (this.time < "6:00" || this.age >= 65 || this.titlee === "12 Angry Men") {
-      return (setTicketPrice * 0.8);
+      return (setTicketPrice * 0.8 * this.quantity);
     } else {
-      return 10;
+      return setTicketPrice * this.quantity;
     }
 }
 
@@ -20,7 +22,8 @@ $(function() {
     var age = parseInt($("input#age").val());
     var movies = $('select#movieTitles').val();
     var time = $('select#time').val();
-    var newTicket = new Ticket (movies,time,age);
+    var quantity = parseInt($('input#quantity').val());
+    var newTicket = new Ticket (movies,time,age,quantity);
     $("#ticketInfo").append("<li>Your age: " + age + "</li>" + "<li>Movie Title: " + movies + "</li>" + "<li>Time: " + time + "</li>");
     $("#ticketPrice").append("<li>$" + newTicket.price() + ".00</li>");
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,25 @@
 describe("Ticket",function(){
   it("creates a ticket that specifies movie title, time, and user's age based on input", function() {
-    var testTicket = new Ticket("Trainspotting", 17, 34)
+    var testTicket = new Ticket("Trainspotting", "5:00", 34, 1)
     expect(testTicket.titlee).to.equal("Trainspotting");
-    expect(testTicket.time).to.equal(17);
+    expect(testTicket.time).to.equal("5:00");
     expect(testTicket.age).to.equal(34);
+    expect(testTicket.quantity).to.equal(1)
   });
   it("creates ticket that calculates twenty percent price discount if user age is over 65",function() {
-    var testTicket = new Ticket("Trainspotting", 17, 70)
+    var testTicket = new Ticket("Trainspotting", "5:00", 70, 1)
     expect(testTicket.price()).to.equal(8);
   });
   it("creates ticket that calculates twenty percent price discount if user attends before 4:00",function() {
-    var testTicket = new Ticket("Trainspotting", "2:00", 34)
+    var testTicket = new Ticket("Trainspotting", "2:00", 34, 1)
     expect(testTicket.price()).to.equal(8);
   });
   it("creates ticket that calculates twenty percent price discount if user attends older movie",function() {
-    var testTicket = new Ticket("12 Angry Men", 5, 34)
+    var testTicket = new Ticket("12 Angry Men", "5:00", 34, 1)
     expect(testTicket.price()).to.equal(8);
+  });
+  it("creates ticket that calculates total price based on quantity",function() {
+    var testTicket = new Ticket("12 Angry Men", "5:00", 34, 2)
+    expect(testTicket.price()).to.equal(16);
   });
 });
