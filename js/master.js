7cd653aa1c4f17f3525f175fb2948439f3e59728
andrew cho and isabelle george <10izzygeorge@gmail.com>
2016-01-26 15:50:21 -0800

    replace tickets if adding more

--- js/master.js
+++ js/master.js
@@ -8,7 +8,6 @@ function Ticket(titlee,time,age, quantity) {
 
 Ticket.prototype.price = function() {
     var setTicketPrice = 10;
-    debugger;
     if (this.time < "6:00" || this.age >= 65 || this.titlee === "12 Angry Men") {
       return (setTicketPrice * 0.8 * this.quantity);
     } else {
@@ -24,7 +23,10 @@ $(function() {
     var time = $('select#time').val();
     var quantity = parseInt($('input#quantity').val());
     var newTicket = new Ticket (movies,time,age,quantity);
+    $('#ticketInfo').empty();
+    $('#ticketPrice').empty();
     $("#ticketInfo").append("<li>Your age: " + age + "</li>" + "<li>Movie Title: " + movies + "</li>" + "<li>Time: " + time + "</li>");
+
     $("#ticketPrice").append("<li>$" + newTicket.price() + ".00</li>");
 
 
