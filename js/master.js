3d056ea380106c5451651f2b2bea75107ac33611
andrew cho and isabelle george <cho-andrew@hotmail.com>
2016-01-26 11:32:13 -0800

    add function to give percentage discount to people over 65.

--- js/master.js
+++ js/master.js
@@ -3,3 +3,10 @@ function Ticket(titlee,time,age) {
   this.time = time;
   this.age = age;
 }
+
+var setTicketPrice = 10
+Ticket.prototype.price = function() {
+    if (this.age >= 65) {
+      return (setTicketPrice * 0.8);
+    }
+  }
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe("Ticket",function(){
   it("creates a ticket that specifies movie title, time, and user's age based on input", function() {
-    var testTicket = new Ticket("Trainspotting", 3, 34)
+    var testTicket = new Ticket("Trainspotting", 5, 34)
     expect(testTicket.titlee).to.equal("Trainspotting");
-    expect(testTicket.time).to.equal(3);
+    expect(testTicket.time).to.equal(5);
     expect(testTicket.age).to.equal(34);
+  });
+  it("creates ticket that calculates twenty percent price discount if user age is over 65",function() {
+    var testTicket = new Ticket("Trainspotting", 5, 70)
+    expect(testTicket.price()).to.equal(8);
   });
 });
 
