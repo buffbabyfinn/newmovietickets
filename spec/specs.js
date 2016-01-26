84f518c97852fb3aae1c64f9549e29c57347c239
andrew cho and isabelle george <10izzygeorge@gmail.com>
2016-01-26 10:22:59 -0800

    add initial files to our repo.

---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+// Example Spec below:
+
+// describe('functionname', function(){
+//   it('first spec here...', function(){
+//     expect(functionname(parameter)).to.equal('placeholder');
+//   });
+// });
