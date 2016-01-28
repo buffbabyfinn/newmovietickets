5c40df8da990b236a885167b81be2e54eb72984c
Megan Fayer and Ronando <student@epicodus.com>
2016-01-28 10:44:40 -0800

    elegant movie ticket code with objects

--- js/master.js
+++ js/master.js
@@ -1,33 +1,46 @@
-function Ticket(titlee,time,age, quantity) {
-  this.titlee = titlee;
+function Ticket(movieName, time, age, quantity) {
+  this.movieName = movieName;
   this.time = time;
   this.age = age;
   this.quantity = quantity;
+  this.movieYear = movieYear;
 }
-
 
 Ticket.prototype.price = function() {
     var setTicketPrice = 10;
-    if (this.time < "6:00" || this.age >= 65 || this.titlee === "12 Angry Men") {
+    if (this.time < "6:00" || this.age >= 65 || this.movieYear < 2000) {
       return (setTicketPrice * 0.8 * this.quantity);
     } else {
       return setTicketPrice * this.quantity;
     }
 }
-
-$(function() {
-  $("#buyButton").click(function(event) {
-    event.preventDefault();
-    var age = parseInt($("input#age").val());
-    var movies = $('select#movieTitles').val();
-    var time = $('select#time').val();
-    var quantity = parseInt($('input#quantity').val());
-    var newTicket = new Ticket (movies,time,age,quantity);
-    $('#ticketInfo').empty();
-    $('#ticketPrice').empty();
-    $("#ticketInfo").append("<li>Your age: " + age + "</li>" + "<li>Movie Title: " + movies + "</li>" + "<li>Time: " + time + "</li>");
-
-    $("#ticketPrice").append("<li>$" + newTicket.price() + ".00</li>");
-
-  });
-});
+//
+// Ticket.prototype.movieYears = function() {
+//   var starWars = { name: "Star Wars", year: 1979 };
+//   var superman = { name: "Superman", year: 2001 };
+//   var spiritedAway = { name: "Spirited Away", year: 1995 };
+//
+//   var movies = { movieList: [starWars, superman, spiritedAway] };
+//
+//   movies.movieList.forEach(function(movie) {
+//         var movieYear = movie.year;
+//         return movieYear;
+//   });
+// }
+//
+// $(function() {
+//   $("#buyButton").click(function(event) {
+//     event.preventDefault();
+//     var age = parseInt($("input#age").val());
+//     var movies = $('select#movieTitles').val();
+//     var time = $('select#time').val();
+//     var quantity = parseInt($('input#quantity').val());
+//     var newTicket = new Ticket (movies,time,age,quantity);
+//     $('#ticketInfo').empty();
+//     $('#ticketPrice').empty();
+//     $("#ticketInfo").append("<li>Your age: " + age + "</li>" + "<li>Movie Title: " + movies + "</li>" + "<li>Time: " + time + "</li>");
+//
+//     $("#ticketPrice").append("<li>$" + newTicket.price() + ".00</li>");
+//
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -6,18 +6,22 @@ describe("Ticket",function(){
     expect(testTicket.age).to.equal(34);
     expect(testTicket.quantity).to.equal(1)
   });
+
   it("creates ticket that calculates twenty percent price discount if user age is over 65",function() {
     var testTicket = new Ticket("Trainspotting", "5:00", 70, 1)
     expect(testTicket.price()).to.equal(8);
   });
+
   it("creates ticket that calculates twenty percent price discount if user attends before 4:00",function() {
     var testTicket = new Ticket("Trainspotting", "2:00", 34, 1)
     expect(testTicket.price()).to.equal(8);
   });
+
   it("creates ticket that calculates twenty percent price discount if user attends older movie",function() {
     var testTicket = new Ticket("12 Angry Men", "5:00", 34, 1)
     expect(testTicket.price()).to.equal(8);
   });
+  
   it("creates ticket that calculates total price based on quantity",function() {
     var testTicket = new Ticket("12 Angry Men", "5:00", 34, 2)
     expect(testTicket.price()).to.equal(16);
